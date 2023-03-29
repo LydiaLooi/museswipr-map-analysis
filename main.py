4d43f598c2d0b1c1887a77e8159d9a37a204c6e2
Lydia Looi <itslydia.looi@gmail.com>
2023-03-30 00:20:14 +1300

    Added first iteration of diff calculation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+import os
 import json
 
 LANE_1_ID = 0
@@ -17,7 +19,7 @@ class MuseSwiprMap:
     def __init__(self, koreograph_asset_filename):
 
         data = None
-        with open(f"{DATA_DIR}/{koreograph_asset_filename}", "r") as f:
+        with open(f"{koreograph_asset_filename}", "r") as f:
             data = json.load(f)
 
         self.title = list(data.keys())[0]
@@ -32,8 +34,9 @@ class MuseSwiprMap:
             note_times_data = t["mEventList"]
             for d in note_times_data:
                 assert d["mStartSample"] == d["mEndSample"]
-                note = Note(int(t["mEventID"]), d["mStartSample"])
-                self.notes.append(note)
+                if t["mEventID"] != "TimingPoint":
+                    note = Note(int(t["mEventID"]), d["mStartSample"])
+                    self.notes.append(note)
         
         self.notes = sorted(self.notes, key=lambda note: note.time)
 
@@ -66,7 +69,48 @@ class MuseSwiprMap:
                 else:
                     file.write("\n")
 
-if __name__ == "__main__":
-    m_map = MuseSwiprMap("Sanguine.asset")
 
-    m_map.output_notes("output.txt")
+def calculate_difficulty(notes):
+    # First, let's sort the notes by time
+    notes = sorted(notes, key=lambda note: note.time)
+
+    # We'll keep track of the total difficulty
+    total_difficulty = 0
+
+    # Now we can calculate the difficulty of each note by looking at the time
+    # difference between it and the previous note, as well as the distance between
+    # the two notes on the playfield
+    for i in range(1, len(notes)):
+        time_diff = notes[i].time - notes[i-1].time
+        lane_diff = abs(notes[i].lane - notes[i-1].lane)
+        difficulty = time_diff + lane_diff
+        total_difficulty += difficulty
+
+    # Finally, we'll return the average difficulty per note
+    return 100000 *(1 / (total_difficulty / (len(notes) - 1)))
+
+if __name__ == "__main__":
+    # get a list of all files in the directory
+    all_files = os.listdir(DATA_DIR)
+
+    # filter out any non-file entries
+    file_list = []
+
+    # walk through all the directories and files in the specified path
+    for root, directories, files in os.walk(DATA_DIR):
+        for filename in files:
+            # construct the full file path
+            file_path = os.path.join(root, filename)
+            # append the file path to the list
+            file_list.append(file_path)
+    
+    with open("difficulties.txt", "w") as f:
+
+        for filename in file_list:
+            try:
+                char = "\\"
+                m_map = MuseSwiprMap(filename)
+                f.write(f"{filename.split(char)[-1].split('.asset')[0]}, {calculate_difficulty(m_map.notes):.2f}\n")
+            except Exception as e:
+                print(f"error parsing file: {filename}: {e}")
+                continue
