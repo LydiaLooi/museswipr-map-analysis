3fbfeb56fde51550d6877adb2755546cf63bba90
Lydia Looi <itslydia.looi@gmail.com>
2023-03-31 01:28:02 +1300

    Zigzags now switch and 5 stream now 2

--- main.py
+++ main.py
@@ -164,9 +164,9 @@ def get_next_pattern_and_required_notes(prev_note: Note, note: Note, time_differ
 
     if notes_per_second >= 5:
         if note.lane != prev_note.lane:
-            return "Zig Zag", 3
+            return "Zig Zag", 2
         else:
-            return "Single Stream", 5
+            return "Single Stream", 2
     elif notes_per_second < 1:
         return "Simple Note", 0
     else:
@@ -187,12 +187,12 @@ def analyze_patterns(notes: List[Note]):
     current_pattern = None
     tolerance = 10 * TIME_CONVERSION / 1000  # 10ms in sample time
 
-    for i in range(1, len(notes)):
+    for i in range(1, len(notes)): # Starts at second note :)
         prev_note = notes[i - 1]
         note = notes[i]
 
         time_difference = note.sample_time - prev_note.sample_time
-        next_pattern_name, next_required_notes = get_next_pattern_and_required_notes(prev_note, note, time_difference)
+        next_pattern_name, next_required_notes = get_next_pattern_and_required_notes(prev_note, note, time_difference) #TODO make changes here that passes in the curent pattern to keep track of n-stacks to get em
 
         if current_pattern and current_pattern.pattern_name == next_pattern_name:
             base_time_difference = current_pattern.notes[1].sample_time - current_pattern.notes[0].sample_time
@@ -201,7 +201,7 @@ def analyze_patterns(notes: List[Note]):
             else:
                 patterns = handle_current_pattern(patterns, current_pattern)
                 current_pattern = Pattern(next_pattern_name, [prev_note, note], next_required_notes)
-        else:
+        else: # If the current pair of notes does not belong to the same pattern as the previous pair of notes
             patterns = handle_current_pattern(patterns, current_pattern)
             current_pattern = Pattern(next_pattern_name, [prev_note, note], next_required_notes)
     patterns = handle_current_pattern(patterns, current_pattern)
@@ -225,21 +225,62 @@ def print_patterns(patterns: List[Pattern]):
 def mini_test():
 
     notes = [
-        Note(0, 0000),
+        Note(0, 0000), #Single stream
         Note(0, 1000),
         Note(0, 2000),
         Note(0, 3000),
-        Note(0, 4000),
+        Note(0, 4000), #Single stream end | zig zag 
         Note(1, 6000),
         Note(0, 8000),
         Note(1, 10000),
         Note(0, 12000),
-        Note(1, 14000),
+        Note(1, 14000), # zig zag end | zig zag start
         Note(0, 15000),
         Note(1, 16000),
         Note(0, 17000),
-        Note(1, 18000)
+        Note(1, 18000), ## zig zag end | Two stack
+        Note(1, 20000), ## two stack end | switch 
+        Note(0, 21000), ## Switch end | three stack 
+        Note(0, 23000),
+        Note(0, 25000), ## Three stack end | switch start
+        Note(1, 26000), ## Switch end | 4 stack start
+        Note(1, 28000),
+        Note(1, 30000),
+        Note(1, 32000), ## four stack end | zig zag
+        Note(0, 34000), 
+        Note(1, 36000), ## zigg zag end | two stack 
+        Note(1, 38000) ## two stack end
     ]
+
+    # notes = [
+    #     Note(0, 0.0000), #Single stream
+    #     Note(0, 0.0227),
+    #     Note(0, 0.0453),
+    #     Note(0, 0.0680),
+    #     Note(0, 0.0907), #Single stream end | zig zag 
+    #     Note(1, 0.1360),
+    #     Note(0, 0.1814),
+    #     Note(1, 0.2268),
+    #     Note(0, 0.2722),
+    #     Note(1, 0.3175), # zig zag end | zig zag start
+    #     Note(0, 0.3402),
+    #     Note(1, 0.3630),
+    #     Note(0, 0.3857),
+    #     Note(1, 0.4084), ## zig zag end | Two stack
+    #     Note(1, 0.4538), ## two stack end | switch 
+    #     Note(0, 0.4765), ## Switch end | three stack 
+    #     Note(0, 0.5220),
+    #     Note(0, 0.5674), ## Three stack end | switch start
+    #     Note(1, 0.5901), ## Switch end | 4 stack start
+    #     Note(1, 0.6355),
+    #     Note(1, 0.6809),
+    #     Note(1, 0.7262), ## four stack end | zig zag
+    #     Note(0, 0.7490), 
+    #     Note(1, 0.7934), ## zigg zag end | two stack 
+    #     Note(1, 0.8387) ## two stack end
+    # ]
+
+
 
 
     p = analyze_patterns(notes)
@@ -282,9 +323,8 @@ if __name__ == "__main__":
     # get a list of all files in the directory
     all_files = os.listdir(DATA_DIR)
 
-    # mini_test()
-
-    run_analysis()
+    mini_test()
+    # run_analysis()
 
     
     # with open("difficulties.txt", "w", encoding="utf-8") as f:
