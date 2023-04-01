b855a8ff52ea0c066f4dc40eae156938df99292f
Lydia Looi <itslydia.looi@gmail.com>
2023-04-01 17:22:45 +1300

    Simple and varying stacks working. Other... mostly working

--- pattern_analysis.py
+++ pattern_analysis.py
@@ -1,5 +1,6 @@
 from entities import Pattern
 from typing import List, Optional
+from abc import ABC, abstractmethod
 
 SWITCH = "Switch"
 ZIG_ZAG = "Zig Zag"
@@ -17,8 +18,8 @@ NOTHING_BUT_THEORY = "Nothing But Theory"
 VARIABLE_STREAM = "Variable Stream"
 OTHER = "Other"
 
-class PatternGroup:
-    def __init__(self, group_name: str, patterns: List[Pattern], start_sample: int, end_sample: int):
+class PatternGroup(ABC):
+    def __init__(self, group_name: str, patterns: List[Pattern], start_sample: int=None, end_sample: int=None):
         self.group_name = group_name
         self.patterns = patterns
         self.start_sample = start_sample
@@ -31,6 +32,82 @@ class PatternGroup:
             p = self.patterns
             extra = ""
         return f"{self.group_name}, {p}{extra}"
+    @abstractmethod
+    def check_pattern(self, current_pattern: Pattern) -> Optional[bool]:
+        pass
+
+    @abstractmethod
+    def reset_group(self, current_pattern: Pattern):
+        pass
+
+    @abstractmethod
+    def is_appendable(self) -> bool:
+        pass
+
+    def is_n_stack(self, pattern: Pattern):
+        return pattern.pattern_name in ("2-Stack", "3-Stack", "4-Stack")
+
+class OtherGroup(PatternGroup):
+
+    def check_pattern(self, current_pattern: Pattern) -> Optional[bool]:
+        # if current_pattern.pattern_name != SIMPLE_NOTE:
+        self.patterns.append(current_pattern)
+        return True
+        # return False
+    
+    def reset_group(self, current_pattern: Pattern):
+        self.patterns = []
+
+    def is_appendable(self) -> bool:
+        return super().is_appendable()
+class SimpleGroup(PatternGroup):
+
+    def check_pattern(self, current_pattern: Pattern) -> Optional[bool]:
+
+        previous_pattern: Optional[Pattern] = self.patterns[-1] if len(self.patterns) > 0 else None
+
+        if current_pattern.pattern_name == SIMPLE_NOTE and (previous_pattern is None or previous_pattern.pattern_name == SIMPLE_NOTE):
+            # print(f"added {current_pattern.pattern_name} to SimpleGroup")
+            self.patterns.append(current_pattern)
+            return True
+        return False
+    
+    def reset_group(self, current_pattern: Pattern):
+        self.patterns = []
+        self.check_pattern(current_pattern)
+
+    def is_appendable(self) -> bool:
+        if len(self.patterns) > 0:
+            for p in self.patterns:
+                if p.pattern_name !=  SIMPLE_NOTE:
+                    raise ValueError(f"Simple Group has a: {p.pattern_name}!!")
+            return True
+        return False
+
+class VaryingStacksGroup(PatternGroup):
+
+    def check_pattern(self, current_pattern: Pattern) -> Optional[bool]:
+
+        previous_pattern: Optional[Pattern] = self.patterns[-1] if len(self.patterns) > 0 else None
+
+        if (previous_pattern is None or self.is_n_stack(previous_pattern)) and self.is_n_stack(current_pattern):
+            self.patterns.append(current_pattern)
+            # print(f"added {current_pattern.pattern_name} to VaryingStacks")
+            return True
+        return False
+    
+    def reset_group(self, current_pattern: Pattern):
+        self.patterns = []
+        self.check_pattern(current_pattern)
+
+    def is_appendable(self) -> bool:
+        if len(self.patterns) >= 2:
+            # Sanity check that everything in it is only N-stacks
+            for p in self.patterns:
+                if not self.is_n_stack(p):
+                    raise ValueError(f"Varying Stack has a: {p.pattern_name}!!")   
+            return True
+        return False
 
 class MapPatternGroups:
 
@@ -43,126 +120,139 @@ class MapPatternGroups:
         self.pattern_group_appended_name: str = ""
 
         self.groups = []
+
+        self.other_group = OtherGroup(OTHER, [])
         self.reset_groups()
 
     def is_n_stack(self, pattern: Pattern):
         return pattern.pattern_name in ("2-Stack", "3-Stack", "4-Stack")
     
-
     def reset_groups(self):
         self.groups = [
-            PatternGroup(SIMPLE_ONLY, [], None, None),
-            PatternGroup(VARYING_STACKS, [], None, None),
-            PatternGroup(OTHER, [], None, None)
+            SimpleGroup(SIMPLE_ONLY, []),
+            VaryingStacksGroup(VARYING_STACKS, [])
         ]
+        self.other_group = OtherGroup(OTHER, [])
 
-    def check_simple_notes(self, group: PatternGroup, current_pattern: Pattern, previous_pattern: Optional[Pattern]):
-        # This should ensure that the Simple Only group only has simple notes
-        if (previous_pattern is None or previous_pattern.pattern_name is SIMPLE_NOTE) and current_pattern.pattern_name == SIMPLE_NOTE :
-            group.patterns.append(current_pattern)
-
-            # Set the start sample initially. Always update the end sample
-            if group.start_sample is None:
-                group.start_sample = current_pattern.notes[0].sample_time
-            group.end_sample = current_pattern.notes[-1].sample_time
-            return True
-
-        # The current pattern is NON-Simple Notes from here on out
-        # We have found a pattern that doesn't fit this group. 
-        if len(group.patterns) > 0:
-            # Sufficient notes in this group to be a valid group
-            for p in group.patterns: # Sanity check that everything in it is only Simple notes
-                if p.pattern_name != SIMPLE_NOTE:
-                    raise ValueError(f"There is a non-simple note in the simple only group: {p.pattern_name}")
-            
-            
-            # Update the end sample to the previous note
-            group.end_sample = previous_pattern.notes[-1].sample_time
-            
-            # Append and reset Other
-            # group.patterns.append(current_pattern)
-            self.groups[-1].patterns = []
-            self.pattern_groups.append(group)
-            self.pattern_group_appended = True
-            self.pattern_group_appended_name = group.group_name
-            return True
-        else: 
-            # Insufficient notes in this group
-            # Reset this group and add the current pattern if valid
-            group.patterns = []
-            if current_pattern.pattern_name == SIMPLE_NOTE:
-                group.patterns.append(current_pattern)
-            
-        
-
-    def check_varying_stacks(self, group: PatternGroup, current_pattern: Pattern, previous_pattern: Optional[Pattern]):
-        # This should ensure that this group only has n stacks
-        if (previous_pattern is None or self.is_n_stack(previous_pattern)) and self.is_n_stack(current_pattern):
-            group.patterns.append(current_pattern)
-            # Set the start sample initially. Always update the end sample
-            if group.start_sample is None:
-                group.start_sample = current_pattern.notes[0].sample_time
-            group.end_sample = current_pattern.notes[-1].sample_time
-            return True
-        
-        # The current pattern should only be NON-n-stacks from here on out
-        if len(group.patterns) >= 2:
-            # Sanity check that everything in it is only N-stacks
-            for p in group.patterns:
-                if self.is_n_stack(p):
-                    raise ValueError("There is a non-N-stack note in the varying stack group :(")   
-
-            self.pattern_groups.append(group)
-            self.pattern_group_appended = True
-            self.pattern_group_appended_name = group.group_name
-            group.end_sample = previous_pattern.notes[-1].sample_time
-            return True
+    def _return_final_groups(self, merge_other=True) -> List[PatternGroup]:
+        if merge_other:
+            new_groups = []
+            current_other = None
+            for pg in self.pattern_groups:
+                if pg.group_name != OTHER:
+                    if current_other is not None:
+                        new_groups.append(current_other)
+                        # TODO, set the start and end times
+                        current_other = None
+                    new_groups.append(pg)
+                else:
+                    if current_other is None:
+                        current_other = OtherGroup(OTHER, [])
+                    current_other.patterns += pg.patterns
+            if current_other is not None:
+                new_groups.append(current_other)
+            return new_groups
         else:
-            # Insufficient notes to be valid group
-            group.patterns = []
-            if self.is_n_stack(current_pattern):
-                group.patterns.append(current_pattern)
-
-
-
-        
+            return self.pattern_groups
 
 
     def identify_pattern_groups(self, patterns_list: List[Pattern]) -> List[PatternGroup]:
-        
+
         # TODO: account f or the duration between patterns too.
-        added = False
-        index_start = 0
         # Lets just first identify Single Notes and Varying Stacks only
     
-
-
-        for i in range(index_start, len(patterns_list)):
+        for i in range(0, len(patterns_list)):
             current_pattern = patterns_list[i]
+            # print(f"\nCurrent pattern: {current_pattern.pattern_name}")
+            added = False # has this pattern been added?
 
-            # Check for the defined patterns
-            for j in range(len(self.groups)):
-                group = self.groups[j]
-                previous_pattern = group.patterns[-1] if len(group.patterns) > 0 else None
+            for group in self.groups:
+                group: PatternGroup
+                _added = group.check_pattern(current_pattern)
+                if _added == True:
+                    added = True
+                else: 
+                    # Check if the group is appendable
+                        if group.is_appendable():
+                            added = True
+                            group_copy = group.__class__(group.group_name, group.patterns, group.start_sample, group.end_sample)
+                            self.pattern_groups.append(group_copy)
+                            self.other_group.reset_group(current_pattern) # reset OtherGroup
+                            # print(f"{type(group_copy).__name__} | Appended {group_copy.group_name} with groups: {group_copy.patterns}")
+                            # Reset all groups with current pattern.
+                            for group in self.groups:
+                                group.reset_group(current_pattern)
 
-                if group.group_name == SIMPLE_ONLY:
-                    added = self.check_simple_notes(group, current_pattern, previous_pattern)
-                elif group.group_name == VARYING_STACKS:
-                    added = self.check_varying_stacks(group, current_pattern, previous_pattern)
+            self.other_group.check_pattern(current_pattern)
+            # print(f"Added = True... Other Group is {self.other_group.patterns}")
 
-            # Always add to the other_group - incase we go through all patterns, and nothing can be added.
-            # This should mean 
-            other_group = self.groups[-1]
-            other_group.patterns.append(current_pattern)
-            if added is False:
-                if len(other_group.patterns) > 1:
-                    # Reset ALL groups
-                    self.pattern_groups.append(other_group)
-                    self.reset_groups()
-                    print("OTHER???")
-                else:
-                    raise ValueError("No pattern was 'added'. Less than 2 patterns are in Other which doesnt seem right.")
+            # We have gone through all the defined groups...
+            if not added: 
+                # Append OtherGroup if no other groups were appendale
+                # print(f"No other group appendable... appending Other with {self.other_group.patterns}")
+                self.pattern_groups.append(OtherGroup(OTHER, self.other_group.patterns, self.other_group.start_sample, self.other_group.end_sample))
+                self.other_group.reset_group(current_pattern) # reset OtherGroup
+                # Reset all groups with current pattern.
+                for group in self.groups:
+                    group.reset_group(current_pattern)
 
-        return self.pattern_groups
+        # Do last check
+        for last__check_group in self.groups:
+            if last__check_group.is_appendable():
+                # print(f"{last__check_group.group_name} is appendable with {last__check_group.patterns}")
+                last_group_copy = last__check_group.__class__(last__check_group.group_name, last__check_group.patterns, last__check_group.start_sample, last__check_group.end_sample)
+                self.pattern_groups.append(last_group_copy)
+                return self._return_final_groups()
+        if len(self.other_group.patterns) > 0: 
+            last_group_copy = OtherGroup(OTHER, self.other_group.patterns, self.other_group.start_sample, self.other_group.end_sample)
+            self.pattern_groups.append(last_group_copy)
+
+        
+
+        return self._return_final_groups()
 
 
+        
+
+
+if __name__ == "__main__":
+    simple = Pattern(SIMPLE_NOTE, [], 0)
+    two = Pattern(TWO_STACK, [], 0)
+    three = Pattern(THREE_STACK, [], 0)
+    four = Pattern(FOUR_STACK, [], 0)
+    switch = Pattern(SWITCH, [], 0)
+    zig_zag = Pattern(ZIG_ZAG, [], 0)
+    stream = Pattern(SINGLE_STREAMS, [], 0)
+
+    groups = MapPatternGroups().identify_pattern_groups([simple])
+    assert len(groups) == 1
+    assert groups[0].group_name == SIMPLE_ONLY
+
+    groups = MapPatternGroups().identify_pattern_groups([two, three])
+    assert len(groups) == 1
+    assert groups[0].group_name == VARYING_STACKS
+
+    groups = MapPatternGroups().identify_pattern_groups([switch])
+    assert len(groups) == 1
+    assert groups[0].group_name == OTHER
+
+    patterns = [simple, simple, two, three, switch, zig_zag, switch, two, zig_zag, stream]
+    groups = MapPatternGroups().identify_pattern_groups(patterns)
+    assert len(groups) == 3
+    assert groups[0].group_name == SIMPLE_ONLY
+    assert groups[1].group_name == VARYING_STACKS
+    assert groups[2].group_name == OTHER
+
+    patterns = [switch, two, switch, two, switch, two]
+    groups = MapPatternGroups().identify_pattern_groups(patterns)
+    assert len(groups) == 1
+    assert groups[0].group_name == OTHER
+
+    # Not quite working when there is an Other of one ----------\/ this one
+    patterns = [stream, switch, stream, switch, two, two, two, switch, simple, switch, stream]
+    groups = MapPatternGroups().identify_pattern_groups(patterns)
+    # assert len(groups) == 5
+
+    print("="*25)
+    for  g in groups:
+        print(f"{g.group_name} | {g.patterns}")
